48c331756810b92132ed4bdd6fc12dd8685266ae
shaig-mahmudov <shaigfm-apa101@code.edu.az>
2026-05-30 21:40:32 +0400

    fix: handle invalid method argument

--- src/main/java/com/engine/taskmanagement/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/engine/taskmanagement/common/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.engine.taskmanagement.common.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
--- src/main/java/com/engine/taskmanagement/common/exception/MethodArgumentNotValidException.java
+++
@@ -1,7 +0,0 @@
-package com.engine.taskmanagement.common.exception;
-
-public class MethodArgumentNotValidException extends RuntimeException {
-    public MethodArgumentNotValidException(String message) {
-        super(message);
-    }
-}
