c17e97ad73c0919514f9b4ecbae31a5f0872f176
shaig-mahmudov <zyphorix.official@gmail.com>
2026-06-03 03:41:37 -0700

    feat: implement due date filtering logic

--- src/main/java/com/engine/taskmanagement/task/dto/request/FilterTaskRequest.java
+++ src/main/java/com/engine/taskmanagement/task/dto/request/FilterTaskRequest.java
@@ -5,10 +5,15 @@ import com.engine.taskmanagement.task.enums.TaskStatus;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class FilterTaskRequest {
     private TaskStatus status;
     private TaskPriority priority;
+    private Boolean dueFromToday;
+    private LocalDate dueDateFrom;
+    private LocalDate dueDateTo;
     private Long projectId;
 }
--- src/main/java/com/engine/taskmanagement/task/specification/TaskSpecification.java
+++ src/main/java/com/engine/taskmanagement/task/specification/TaskSpecification.java
@@ -5,6 +5,7 @@ import com.engine.taskmanagement.task.entity.Task;
 import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class TaskSpecification {
     public static Specification<Task> filter(FilterTaskRequest request) {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
+            LocalDate today = LocalDate.now();
 
             predicates.add(criteriaBuilder.isNull(root.get("deletedAt")));
 
@@ -32,6 +34,29 @@ public class TaskSpecification {
                 predicates.add(
                         criteriaBuilder.equal(root.get("project").get("id"), request.getProjectId())
                 );
+            }
+
+            if (Boolean.TRUE.equals(request.getDueFromToday()) && request.getDueDateTo() != null) {
+                predicates.add(
+                        criteriaBuilder.greaterThanOrEqualTo(root.get("dueDate"), today)
+                );
+
+                predicates.add(
+                        criteriaBuilder.lessThanOrEqualTo(root.get("dueDate"), request.getDueDateTo())
+                );
+            } else {
+
+                if (request.getDueDateFrom() != null) {
+                    predicates.add(
+                            criteriaBuilder.greaterThanOrEqualTo(root.get("dueDate"), request.getDueDateFrom())
+                    );
+                }
+
+                if (request.getDueDateTo() != null) {
+                    predicates.add(
+                            criteriaBuilder.lessThanOrEqualTo(root.get("dueDate"), request.getDueDateTo())
+                    );
+                }
             }
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
