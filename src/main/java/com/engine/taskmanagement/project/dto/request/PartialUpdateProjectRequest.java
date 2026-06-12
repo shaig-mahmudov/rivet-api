14705209f8c4753aa7220bc0b580e02ee251c063
shaig-mahmudov <shaigfm-apa101@code.edu.az>
2026-06-12 17:55:18 +0400

    fix: add project request validation limits

--- src/main/java/com/engine/taskmanagement/project/dto/request/PartialUpdateProjectRequest.java
+++ src/main/java/com/engine/taskmanagement/project/dto/request/PartialUpdateProjectRequest.java
@@ -1,11 +1,15 @@
 package com.engine.taskmanagement.project.dto.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class PartialUpdateProjectRequest {
+    @Size(max = 100, message = "Project Name cannot exceed 100 characters")
     private String name;
+
+    @Size(max = 250, message = "Project Description cannot exceed 250 characters")
     private String description;
 }
--- src/main/java/com/engine/taskmanagement/project/dto/request/UpdateProjectRequest.java
+++ src/main/java/com/engine/taskmanagement/project/dto/request/UpdateProjectRequest.java
@@ -1,11 +1,17 @@
 package com.engine.taskmanagement.project.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class UpdateProjectRequest {
+    @NotBlank(message = "Project Name is required")
+    @Size(max = 100, message = "Project Name cannot exceed 100 characters")
     private String name;
+
+    @Size(max = 250, message = "Project Description cannot exceed 250 characters")
     private String description;
 }
